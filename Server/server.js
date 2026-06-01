1b59b806b5245154a5f11be8f65bb00eb37c79a1
Sarang Akhade <Akhadesarang1@gmail.com>
2026-06-01 13:54:07 +0530

    fix(merge): resolve conflicts with rohith-update branch

--- Server/server.js
+++ Server/server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-const bcrypt = require("bcryptjs");
+const bcrypt = require("bcryptjs"); 
 const jwt = require("jsonwebtoken");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
